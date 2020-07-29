f823169ee6be00b18cc6b0459b8378f21d663d14
Sami Elkhayri <selkhayri@gmail.com>
2020-07-29 13:18:48 -0400

    Commented R script + completed writeup

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,30 +6,48 @@ library(tidyverse)
 #
 ###########################
 
+# Load the MechaCar_mpg dataset
 dataset <- 'MechaCar_mpg.csv'
-
 mech_data <- read.csv(dataset)
 
+# Show the first few records of the mech_data dataframe
 print(head(mech_data))
 
-mlr <- lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance, data = mech_data, na.action=na.omit)
-
-print(summary(mlr))
-
-# Add AWD to independent variables
-
+# Perform Multiple Linear Regression Analysis
+# Use vehicle.length, vehicle.weight, spoiler.angle, ground.clearance, and AWD
 mlr <- lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD, data = mech_data, na.action=na.omit)
 
+# Display the summary statistics of the model
 print(summary(mlr))
 
-# Pare down independent variables to vehicle.length, vehicle.weight, and ground.clearance
+# Perform Multiple Linear Regression Analysis - Another combination
+# Use vehicle.length, vehicle.weight, spoiler.angle, ground.clearance
+mlr <- lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance, data = mech_data, na.action=na.omit)
 
+# Display the summary statistics of the model
+print(summary(mlr))
+
+# Perform Multiple Linear Regression Analysis - Yet Another combination
+# Pare down independent variables to vehicle.length, vehicle.weight, and ground.clearance
 mlr <- lm(mpg ~ vehicle.length + vehicle.weight + ground.clearance, data = mech_data, na.action=na.omit)
 
+# Display the summary statistics of the model
 print(summary(mlr))
 
 # Summary statistics of quantitative variables
 quantitative <- mech_data[,c("vehicle.length","vehicle.weight","spoiler.angle","ground.clearance")]
+
+print("Suummary Statistics of quantitative variables")
+print("=============================================")
+print("Mean")
+print(sapply(quantitative,mean))
+print("Median")
+print(sapply(quantitative,median))
+print("Variance")
+print(sapply(quantitative,var))
+print("Standard Deviation")
+print(sapply(quantitative,sd))
+
 
 ###########################
 #
@@ -37,18 +55,22 @@ quantitative <- mech_data[,c("vehicle.length","vehicle.weight","spoiler.angle","
 #
 ###########################
 
+# Load the Suspension_Coil dataset
 dataset <- 'Suspension_Coil.csv'
-
 susp_data <- read.csv(dataset)
 
+# Display the first few records of the Suspension_Coil dataset
 print(head(susp_data))
 
+# Separate the Suspension_Coil data into three parts based on the Manufacturing_Lot column: Lot1, Lot2, and Lot3
 lot1 <- susp_data[susp_data$Manufacturing_Lot == "Lot1",]$PSI
 lot2 <- susp_data[susp_data$Manufacturing_Lot == "Lot2",]$PSI
 lot3 <- susp_data[susp_data$Manufacturing_Lot == "Lot3",]$PSI
 
+# stats
+# -----
+# This function outputs to the terminal the mean, median, variance, and standard deviation of the data column that is passed in.
 stats <- function(col,colname) {
-  
   m_mean <- paste("Mean: ",mean(col))
   m_median <- paste("Median: ",median(col))
   m_var <- paste("Variance: ",var(col))
